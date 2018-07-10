5f04e0767cad33658b6c0e045b3767073e60305b
Ethan Gyori <37253012+EthanGyori@users.noreply.github.com>
2018-07-10 08:57:39 -0500

    Update Password.ps1
    Provided "else" statements to force switch types to be converted to object types. Removes bug where "false" may be added to the generated password.

--- Password.ps1
+++ Password.ps1
@@ -14,7 +14,11 @@ Last Modified: 07/10/2018
 
 param([int]$size, [switch]$a, [switch]$n, [switch]$s, [switch]$e)
 if($a){rv a;$a = [char[]]([char]65..[char]90) + [char[]]([char]97..[char]122)}
+else{rv a; $a}
 if($n){rv n;$n = [char[]]([char]48..[char]57)}
+else{rv n; $n}
 if($s){rv s;$s = [char[]]([char]33..[char]47)}
+else{rv s; $s}
 if($e){rv e;$e = [char[]]([char]122..[char]126)}
+else{rv e; $e}
 ($a+$n+$s+$e | sort {Get-Random})[0..$size] -join ''  
