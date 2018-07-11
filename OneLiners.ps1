dbbaa462f0fc8e9a8238fb71266db0a48dea4709
Ethan Gyori <37253012+EthanGyori@users.noreply.github.com>
2018-07-11 08:55:21 -0500

    Update OneLiners.ps1

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -4,3 +4,6 @@ ls | %{(cat $_.name) -replace 'old string','new string'| sc $_.name}
 
 #replace each occurance of a string in a file name for each file in directory
 gci | ren -n {$_.name -replace 'old','new'}
+
+#import every task in cd into task scheduler where extension is .xml
+ls |?{$_.Extension -eq '.xml'} | %{schtasks /create /tn $_.BaseName /xml $_.FullName}
