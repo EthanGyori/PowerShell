5988e42cdb349ef1b25b9085be38a6eb31b357fc
Ethan Gyori <37253012+EthanGyori@users.noreply.github.com>
2018-07-11 13:00:07 -0500

    Update OneLiners.ps1

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -1,12 +1,17 @@
-<#--------------------
---|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|--
---|Good One-Liners|--
---|  Written by:  |--
---|  Ethan Gyori  |--
---| ~ ~ ~ ~ ~ ~ ~ |--
---|   e n j o y   |--
---|_______________|--
---------------------#>
+<#         /////////////////
+          /////////////////
+  |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|///////
+  |Good One-Liners|//////
+  |  Written by:  |/////
+  |  Ethan Gyori  |////
+  | ~ ~ ~ ~ ~ ~ ~ |///
+  |   e n j o y   |//
+  |_______________|/
+         |:|///
+         |:|//
+         |:|/
+          ‾
+#>
 
 #replace each occurance of string in file for each file in directory
 ls | %{(cat $_.name) -replace 'old string','new string'| sc $_.name}
