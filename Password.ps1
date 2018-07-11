675a58c1620203071ac424c93962b510b4c7f1ed
Ethan Gyori <37253012+EthanGyori@users.noreply.github.com>
2018-07-11 11:38:27 -0500

    Version 2 of Password.ps1
    Removed redundancy and increased dynamics.
    -Redundant and unnecessary variables replaced for character collection and string preparation
    -Moved accessing [char[]] into a function
    -no longer reliant on empty string for no default values error, moved to try/catch exception series

--- Password.ps1
+++ Password.ps1
@@ -13,12 +13,7 @@ Last Modified: 07/10/2018
 -------------------------#>
 
 param([int]$size, [switch]$a, [switch]$n, [switch]$s, [switch]$e)
-if($a){rv a;$a = [char[]]([char]65..[char]90) + [char[]]([char]97..[char]122)}
-else{rv a; $a}
-if($n){rv n;$n = [char[]]([char]48..[char]57)}
-else{rv n; $n}
-if($s){rv s;$s = [char[]]([char]33..[char]47)}
-else{rv s; $s}
-if($e){rv e;$e = [char[]]([char]122..[char]126)}
-else{rv e; $e}
-(''+$a+$n+$s+$e | sort {Get-Random})[0..$size] -join ''  
+function c{param ($x, $y)
+return ([char[]]([char]$x..[char]$y))}
+
+try{(($a,65,90),($a,97,122),($n,48,57),($s,33,47),($e,122,126)| %{if($_[0]){c $_[1] $_[2]}}|sort {Get-Random})[0..$size] -join ''}catch{}
