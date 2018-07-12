a573165d01bc3e4644b5f2c936757583ff329082
Ethan Gyori <37253012+EthanGyori@users.noreply.github.com>
2018-07-12 14:45:03 -0500

    Update OneLiners.ps1

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -21,3 +21,7 @@ gci | ren -n {$_.name -replace 'old','new'}
 
 #import every task in cd into task scheduler where extension is .xml
 ls | ?{$_.Extension -eq '.xml'} | %{schtasks /create /tn $_.BaseName /xml $_.FullName}
+
+#creates spinning graphic for loading or in process tasks
+ $a = [enum]::getvalues([system.consolecolor]);while($?){'|','/','-','\' | %{Write-Host "$_`b" -n -f ($a[(Get-Random -ma $a.length)]);sleep -m 50}}
+ 
