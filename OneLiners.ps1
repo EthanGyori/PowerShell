c4283b985824ca0b00f62d4c073a46a84fc295e0
Ethan Gyori <37253012+EthanGyori@users.noreply.github.com>
2019-03-01 15:25:11 -0600

    Update OneLiners.ps1

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -13,18 +13,19 @@
           ‾
 #>
 
-#replace each occurance of string in file for each file in directory
+#replace each occurrence of string in file for each file in directory
 ls | %{(cat $_.name) -replace 'old string','new string'| sc $_.name}
 
-#replace each occurance of a string in a file name for each file in directory
+#replace each occurrence of a string in a file name for each file in directory
 gci | ren -n {$_.name -replace 'old','new'}
 
 #import every task in cd into task scheduler where extension is .xml
 ls | ?{$_.Extension -eq '.xml'} | %{schtasks /create /tn $_.BaseName /xml $_.FullName}
 
 #shutdown logs w/ device,user, and timestamp
-Get-EventLog -lo system -n 1000 | ?{$_.eventid –eq ‘1074’} | Format-Table machinename, username, timegenerated –a
-         # 1074   shutdown
+ get-eventlog -list| %{get-eventlog -LogName $_.Log -n 100 -ErrorAction SilentlyContinue} | ?{$_.EventID -in
+ $id} | Format-Table machinename, username, timegenerated,eventid –a
+         # $id = 1074   shutdown
          # 7001   login
          # 7002   logout
          # 4728, 4732, 4756         privlage escalation
